1e0461940170a9b619b48fd93c41bde05d1362b9
MatiasRosales <matias.rosales97@hotmail.com>
2019-11-12 18:11:28 -0300

    las fechas se ingresaran como string en formato  DD/MM/AAAA

--- Script creacion tablas.sql
+++ Script creacion tablas.sql
@@ -903,22 +903,22 @@ GO
 --funciones de listados estadisticos, ambos reciben 2 fechas q seran ingresadas desde la aplicacion
 
 
-create function [GDDS2].listadoEstadisticoProveedoresMayorDescuento(@fecha1 datetime , @fecha2 datetime)
+create function [GDDS2].listadoEstadisticoProveedoresMayorDescuento(@fecha1 nvarchar(50) , @fecha2 nvarchar(50))
 returns table
 as 
 return 
-select TOP 5 prov.id_proveedor PROVEEDOR,ru.rubr_detalle RUBRO,prov_razon_social RAZON_SOCIAL, prov.prov_CUIT CUIT, prov.prov_email EMAIL, prov.prov_telefono TELEFONO, prov.prov_contacto CONTACTO,(select  top 1(convert( nvarchar(10),cast(((ofe.ofer_pr_lista - ofe.ofer_pr_oferta )/ofe.ofer_pr_lista)*100 as decimal(12,2) ))+'%') from GDDS2.Proveedor p2 join GDDS2.Oferta ofe on ofe.id_proveedor = p2.id_proveedor where p2.id_proveedor =prov.id_proveedor and ofe.ofer_f_public >= @fecha1 and ofe.ofer_f_public <= @fecha2  order by 1 desc) PORCENTAJE_MAS_ALTO
+select TOP 5 prov.id_proveedor PROVEEDOR,ru.rubr_detalle RUBRO,prov_razon_social RAZON_SOCIAL, prov.prov_CUIT CUIT, prov.prov_email EMAIL, prov.prov_telefono TELEFONO, prov.prov_contacto CONTACTO,(select  top 1(convert( nvarchar(10),cast(((ofe.ofer_pr_lista - ofe.ofer_pr_oferta )/ofe.ofer_pr_lista)*100 as decimal(12,2) ))+'%') from GDDS2.Proveedor p2 join GDDS2.Oferta ofe on ofe.id_proveedor = p2.id_proveedor where p2.id_proveedor =prov.id_proveedor and ofe.ofer_f_public between (convert(datetime,convert(datetime,@fecha1,103),120)) and (convert(datetime,convert(datetime,@fecha2,103),120))   order by 1 desc) PORCENTAJE_MAS_ALTO
 from GDDS2.Proveedor prov join GDDS2.Rubro ru on ru.rubr_id = prov.rubr_id 
 where prov.prov_activo = 1 
 order by 8 desc
 
 GO
 
-create function [GDDS2].listadoEstadisticoMayorFacturacion(@fecha1 datetime, @fecha2 datetime)
+create function [GDDS2].listadoEstadisticoMayorFacturacion(@fecha1 nvarchar(50), @fecha2 nvarchar(50))
 returns table
 as 
 return 
-select TOP 5 prov.id_proveedor PROVEEDOR,ru.rubr_detalle RUBRO,prov_razon_social RAZON_SOCIAL, prov.prov_CUIT CUIT, prov.prov_email EMAIL, prov.prov_telefono TELEFONO, prov.prov_contacto CONTACTO, (select isnull(sum(f.fact_importe),0) from GDDS2.Factura f where f.id_proveedor = prov.id_proveedor and f.fact_fecha >= @fecha1 and f.fact_fecha <= @fecha2) TOTAL_FACTURADO
+select TOP 5 prov.id_proveedor PROVEEDOR,ru.rubr_detalle RUBRO,prov_razon_social RAZON_SOCIAL, prov.prov_CUIT CUIT, prov.prov_email EMAIL, prov.prov_telefono TELEFONO, prov.prov_contacto CONTACTO, (select isnull(sum(f.fact_importe),0) from GDDS2.Factura f where f.id_proveedor = prov.id_proveedor and f.fact_fecha  between (convert(datetime,convert(datetime,@fecha1,103),120)) and (convert(datetime,convert(datetime,@fecha2,103),120))             ) TOTAL_FACTURADO
 from GDDS2.Proveedor prov join GDDS2.Rubro ru on ru.rubr_id = prov.rubr_id 
 where prov.prov_activo = 1 
 order by 8 desc
